6f6a906604a58a53b599c726d30a88e57d176633
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-10-29 11:46:25 -0700

    Create initial config package

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,36 @@
 package config
+
+import (
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	DBConfig DatabaseConfig
+	APIConfig APIConfig
+}
+
+type DatabaseConfig struct {
+	DSN string
+}
+
+type APIConfig struct {
+	Port string
+}
+
+func LoadConfig() (*Config, error) {
+	// Initially load from env vars
+	dsn := os.Getenv("DB_DSN")
+	if dsn == "" {
+		return nil, fmt.Errorf("DB_DSN environment variable is not set")
+	}
+
+	return &Config{
+		DBConfig: DatabaseConfig{
+			DSN: dsn,
+		},
+		APIConfig: APIConfig{
+			Port: "5000",
+		},
+	}, nil
+}
